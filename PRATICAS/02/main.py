35d30a87e2bc048b2580ece8110df40af452be53
Romadeoliveira3 <romadeoliveira3@gmail.com>
2023-06-25 00:11:07 -0300

    Documento Alerta Academico atualizado

---
+++ PRATICAS/02/main.py
@@ -0,0 +1,10 @@
+###----Lista_02----###
+
+# Disciplina: IF264 - Métodos Computacionais
+# Professore: Paulo Freitas
+# Discentes:  Romário Jonas de Oliveira Veloso
+#             Felipe Pontes
+
+
+
+#-----Problema 1-----#:
